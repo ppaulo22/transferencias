cc746441f25567b7ccab03262e45eadfa480e93a
Pedro Paulo dos Santos <pedro2206@live.com>
2022-01-17 21:50:04 -0300

    Sistema de transferências e extrato

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
-import { NgModule } from '@angular/core';
+import { AppRouterModule } from './app-routing.module';
+import { registerLocaleData } from '@angular/common';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ExtratoComponent } from './extrato/extrato.component';
+import { NovaTransferenciaComponent } from './nova-transferencia/nova-transferencia.component';
+import localePt from '@angular/common/locales/pt';
+import { HttpClientModule } from '@angular/common/http';
 
+registerLocaleData(localePt, 'pt')
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [AppComponent, NovaTransferenciaComponent, ExtratoComponent],
+  imports: [BrowserModule, FormsModule, HttpClientModule, AppRouterModule],
+  providers: [
+    {provide: LOCALE_ID, useValue: 'pt' },
+    {
+      provide: DEFAULT_CURRENCY_CODE,
+      useValue:'BRL',
+    },
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
